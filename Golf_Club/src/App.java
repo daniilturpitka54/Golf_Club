a7db0c5c3d7790551aca1e040b61f8810602bc31
daniilturpitka54 <daniil.turpitka@gmail.com>
2023-11-05 17:46:23 -0800

    added teetime comand

--- Golf_Club/src/App.java
+++ Golf_Club/src/App.java
@@ -1,9 +1,13 @@
 import java.util.HashMap;
+import java.util.Date;
 
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         HashMap<String, Member> members = new HashMap<String, Member>();
+        HashMap<Integer, TeeTime> teetimes = new HashMap<Integer, TeeTime>();
+        HashMap<Date, TeeTime> tthourh = new HashMap<Date, TeeTime>();
+        int teeTimeIds = 1;
         String[][] comands = { { "ADD_GOLFER", "Mr.", "Tiger Woods", "36.4" },
                 { "ADD_GOLFER", "Mr.", "Victor Hovland", "12.7" }, { "ADD_SCORE", "Victor Hovland", "102" },
                 { "ADD_SCORE", "Tiger Woods", "90" },
@@ -12,7 +16,8 @@ public class App {
                 { "ADD_SCORE", "Tiger Woods", "96" },
                 { "GET_AVERAGE", "Tiger Woods" },
                 { "GET_BEST", "Tiger Woods" },
-                { "GET_BEST", "Victor Hovland" }
+                { "GET_BEST", "Victor Hovland" },
+                { "NEW_TEETIME", "Tiger Woods, Daniil Turpitka", "2023-11-25/13-45", "2" }
         };
         for (int i = 0; i < comands.length; i++) {
             String comand = comands[i][0];
@@ -28,7 +33,14 @@ public class App {
             } else if (comand == "GET_BEST") {
                 Member temp = members.get(comands[i][1]);
                 System.out.println(temp.bestScore());
+            } else if (comand == "NEW_TEETIME") {
+                TeeTime tt = new TeeTime(comands[i][1], comands[i][2], comands[i][3], teeTimeIds);
+                teetimes.put(teeTimeIds, tt);
+                teeTimeIds++;
+                System.out.println(tt.date);
+
             }
+
         }
 
     }
--- Golf_Club/src/TeeTime.java
+++ Golf_Club/src/TeeTime.java
@@ -3,20 +3,21 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 
 public class TeeTime {
-    int[] players = { 0, 0, 0, 0 };
+    String[] players = { "", "", "", "" };
     Date date;
     int resId;
 
     int carts;
 
-    TeeTime(int[] ps, String dt, int cts, int rid) {
-        for (int i = 0; i < players.length; i++) {
-            players[i] = ps[i];
+    TeeTime(String ps, String dt, String cts, int rid) {
+        String[] pls = ps.split(", ", 4);
+        for (int i = 0; i < pls.length; i++) {
+            players[i] = pls[i];
         }
         resId = rid;
-        carts = cts;
+        carts = Integer.parseInt(cts);
         try {
-            SimpleDateFormat dateForm = new SimpleDateFormat("yyyy-mm-dd-HH-MM");
+            SimpleDateFormat dateForm = new SimpleDateFormat("yyyy-MM-dd/HH-mm");
             date = dateForm.parse(dt);
         } catch (java.text.ParseException exc) {
             System.out.println(exc);
